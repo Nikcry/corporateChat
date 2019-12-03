043624d382275f0d424d55d2cde70d3b07c6e70e
Поляков <polyakov@scrum.ru>
2019-12-03 13:00:00 +0300

    add runnable class

--- ChatServer.java
+++ ChatServer.java
@@ -32,6 +32,66 @@ public class ChatServer {
     }
 
     private class SocketProcessor implements Runnable{
+        Socket s;
+        BufferedReader br;
+        BufferedWriter bw;
 
+        SocketProcessor(Socket socketParam) throws IOException {
+            s = socketParam;
+            br = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
+            bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream(), "UTF-8") );
+        }
+
+        public void run() {
+            while (!s.isClosed()) {
+                String line = null;
+                try {
+                    line = br.readLine();
+                } catch (IOException e) {
+                    close();
+                }
+
+                if (line == null) {
+                    close();
+                } else if ("shutdown".equals(line)) {
+                    serverThread.interrupt();
+                    try {
+                        new Socket("localhost", port);
+                    } catch (IOException ignored) {
+                    } finally {
+                        shutdownServer();
+                    }
+                } else {
+                    for (SocketProcessor sp:q) {
+                        sp.send(line);
+                    }
+                }
+            }
+        }
+
+        public synchronized void send(String line) {
+            try {
+                bw.write(line);
+                bw.write("\n");
+                bw.flush();
+            } catch (IOException e) {
+                close();
+            }
+        }
+
+        public synchronized void close() {
+            q.remove(this);
+            if (!s.isClosed()) {
+                try {
+                    s.close();
+                } catch (IOException ignored) {}
+            }
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            close();
+        }
     }
 }
