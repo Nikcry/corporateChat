609d83980cf5f8ae32507b0933928755c7964a8b
Терпугов <terpugov@scrum.ru>
2019-12-04 19:00:00 +0300

    add client class

---
+++ ChatClient.java
@@ -0,0 +1,34 @@
+import java.net.Socket;
+import java.io.*;
+
+public class ChatClient {
+    final Socket s;
+    final BufferedReader socketReader;
+    final BufferedWriter socketWriter;
+    final BufferedReader userInput;
+
+    public ChatClient(String host, int port) throws IOException {
+
+    }
+
+
+    public void run() {
+
+    }
+
+
+    public synchronized void close() {
+
+    }
+
+    public static void main(String[] args)  {
+
+    }
+
+
+    private class Receiver implements Runnable{
+
+        public void run() {
+
+    }
+}
